8d844322a3eeb639ebfacc0c37bcc08a34b09def
Alex <alexhors@uw.edu>
2017-05-13 10:53:40 -0700

    forgot some files

--- src/main/java/barbot/database/service/UserServiceImpl.java
+++ src/main/java/barbot/database/service/UserServiceImpl.java
@@ -1,47 +1,44 @@
 package barbot.database.service;
 
-import java.util.List;
+import javax.transaction.Transactional;
 
-import javax.annotation.Resource;
-
+import barbot.database.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import barbot.database.model.User;
-import barbot.database.repository.UserRepository;
 
 /**
  * Created by Naveen on 4/13/17.
  */
 @Service
 @Component
+@Transactional
 public class UserServiceImpl implements UserService, UserDetailsService {
 
-    @Resource
-    private UserRepository userRepository;
-
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    @Autowired
+    UserDao userDao;
 
     @Override
-    @Transactional
     public User findById(String userId) {
         Assert.hasLength(userId, "UserId must not be empty");
-        return this.userRepository.findByUid(userId);
+        return userDao.findByUid(userId);
     }
 
     @Override
-    @Transactional
     public User findByName(String name) {
         Assert.hasLength(name, "Username must not be empty");
-        List<User> users = this.userRepository.findByName(name);
-        return users.isEmpty() ? null : users.get(0);
+        return userDao.findByName(name);
+    }
+
+    @Override
+    public void create(User user) {
+        userDao.save(user);
     }
 
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
