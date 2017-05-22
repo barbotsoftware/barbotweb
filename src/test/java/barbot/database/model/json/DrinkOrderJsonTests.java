e09ba72fd92f81114526a1b3701f1ff6e5c0d3e8
Naveen Yadav <nyadav@uw.edu>
2017-05-21 23:03:28 -0700

    Moved JsonTests into model.json package

---
+++ src/test/java/barbot/database/model/json/DrinkOrderJsonTests.java
@@ -0,0 +1,83 @@
+package barbot.database.model.json;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import barbot.database.model.DrinkOrder;
+import barbot.database.model.View;
+
+/**
+ * Created by Naveen on 4/25/17.
+ */
+@RunWith(SpringRunner.class)
+@JsonTest
+public class DrinkOrderJsonTests extends BaseJsonTests {
+
+    @Autowired
+    private JacksonTester<DrinkOrder> jacksonTester;
+
+    String drinkOrderRequestJson;
+    String drinkOrderResponseJson;
+
+    DrinkOrder drinkOrderRequest;
+    DrinkOrder drinkOrderResponse;
+
+    @Before
+    public void setUp() {
+        drinkOrderRequestJson = "";
+
+        drinkOrderResponseJson = "{\n" +
+                "  \"drink_order_id\":\"drinkorder_5e0f58\"\n" +
+                "}";
+
+        drinkOrderRequest = new DrinkOrder();
+
+        drinkOrderResponse = new DrinkOrder("drinkorder_5e0f58");
+    }
+
+    @Test
+    public void testSerializeRequest() throws Exception {
+        // TODO: finish request tests
+    }
+
+    @Test
+    public void testDeserializeRequest() throws Exception {
+
+    }
+
+    @Test
+    public void testSerializeResponse() throws Exception {
+        // Set View = Response
+        useView(View.Response.class, jacksonTester);
+
+        JsonContent<DrinkOrder> result = this.jacksonTester.write(drinkOrderResponse);
+
+        // Compare DrinkOrder Object to Json
+        assertThat(result).isEqualToJson("drinkorderresponse.json");
+
+        // Check Id
+        assertThat(result).hasJsonPathStringValue("@.drink_order_id");
+        assertThat(result).extractingJsonPathStringValue("@.drink_order_id")
+                .isEqualTo("drinkorder_5e0f58");
+    }
+
+    @Test
+    public void testDeserializeResponse() throws Exception {
+        // Set View = Response
+        useView(View.Response.class, jacksonTester);
+
+        assertThat(this.jacksonTester.parse(drinkOrderResponseJson))
+                .isEqualTo(drinkOrderResponse);
+
+        // Check Id
+        assertThat(this.jacksonTester.parseObject(drinkOrderResponseJson).getUid()).isEqualTo("drinkorder_5e0f58");
+    }
+}
---
+++ src/test/java/barbot/database/model/json/RecipeJsonTests.java
@@ -0,0 +1,224 @@
+package barbot.database.model.json;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import barbot.database.model.Ingredient;
+import barbot.database.model.Recipe;
+import barbot.database.model.View;
+
+/**
+ * Created by Naveen on 4/15/17.
+ */
+@RunWith(SpringRunner.class)
+@JsonTest
+public class RecipeJsonTests extends BaseJsonTests {
+
+    @Autowired
+    private JacksonTester<Recipe> jacksonTester;
+
+    String recipeIdJson;
+    String recipeSummaryJson;
+    String recipeDetailJson;
+
+    Recipe recipeId;
+    Recipe recipeSummary;
+    Recipe recipeDetail;
+
+    Set<Ingredient> ingredients;
+
+    @Before
+    public void setUp() {
+        recipeIdJson = "{\n" +
+                "  \"recipe_id\":\"recipe_123456\"\n" +
+                "}";
+
+        recipeSummaryJson = "{ \"name\": \"Cuba Libre\"," +
+                "  \"recipe_id\": \"recipe_8a4d7a\"," +
+                "  \"img\": \"http:\\/\\/192.168.1.41\\/barbot\\/public\\/images\\/cubalibre.png\" }";
+
+        recipeDetailJson = "{" +
+                "            \"name\": \"The Sour\"," +
+                "            \"recipe_id\": \"recipe_9aa19a\"," +
+                "            \"img\": \"http:\\/\\/farm8.staticflickr.com\\/7252\\/7594170156_46bf574865_o.jpg\"," +
+                "            \"ingredients\": [{" +
+                "                \"ingredient_id\": \"ingredient_123450\"," +
+                "                \"name\": \"Ingredient0\"" +
+                "            }, {" +
+                "                \"ingredient_id\": \"ingredient_123451\"," +
+                "                \"name\": \"Ingredient1\"" +
+                "            }, {" +
+                "                \"ingredient_id\": \"ingredient_123452\"," +
+                "                \"name\": \"Ingredient2\"" +
+                "            }, {" +
+                "                \"ingredient_id\": \"ingredient_123453\"," +
+                "                \"name\": \"Ingredient3\"" +
+                "            }]" +
+                "}";
+
+        recipeId = new Recipe("recipe_123456");
+
+        recipeSummary = new Recipe("recipe_8a4d7a",
+                "Cuba Libre",
+                "http://192.168.1.41/barbot/public/images/cubalibre.png");
+
+        ingredients = new HashSet<>();
+
+        for (int i = 0; i < 4; i++) {
+            Ingredient ingredient = new Ingredient("ingredient_12345" + i, "Ingredient" + i);
+
+            ingredients.add(ingredient);
+        }
+
+        recipeDetail = new Recipe("recipe_9aa19a",
+                "The Sour",
+                "http://farm8.staticflickr.com/7252/7594170156_46bf574865_o.jpg",
+                ingredients);
+    }
+
+    @Test
+    public void testSerializeId() throws Exception {
+
+        // Set View = Id
+        useView(View.Id.class, jacksonTester);
+
+        JsonContent<Recipe> result = this.jacksonTester.write(recipeId);
+
+        // Compare Recipe Object to Json
+        assertThat(result).isEqualToJson("recipeid.json");
+
+        // Check Id
+        assertThat(result).hasJsonPathStringValue("@.recipe_id");
+        assertThat(result).extractingJsonPathStringValue("@.recipe_id")
+                .isEqualTo("recipe_123456");
+    }
+
+    @Test
+    public void testDeserializeId() throws Exception {
+
+        // Set View = Id
+        useView(View.Id.class, jacksonTester);
+
+        // Parse Json and compare with Recipe Object
+//        assertThat(this.jacksonTester.parse(recipeIdJson))
+//                .isEqualTo(recipeId);
+
+        // Check Id
+        assertThat(this.jacksonTester.parseObject(recipeIdJson).getUid()).isEqualTo("recipe_123456");
+    }
+
+    @Test
+    public void testSerializeSummary() throws Exception {
+
+        // Set View = Summary
+        useView(View.Summary.class, jacksonTester);
+
+        JsonContent<Recipe> result = this.jacksonTester.write(recipeSummary);
+
+        // Compare Recipe Object to Json
+        assertThat(result).isEqualToJson("recipesummary.json");
+
+        // Check Id
+        assertThat(result).hasJsonPathStringValue("@.recipe_id");
+        assertThat(result).extractingJsonPathStringValue("@.recipe_id")
+                .isEqualTo("recipe_8a4d7a");
+
+        // Check Name
+        assertThat(result).hasJsonPathStringValue("@.name");
+        assertThat(result).extractingJsonPathStringValue("@.name")
+                .isEqualTo("Cuba Libre");
+
+        // Check Img
+        assertThat(result).hasJsonPathStringValue("@.img");
+        assertThat(result).extractingJsonPathStringValue("@.img")
+                .isEqualTo("http://192.168.1.41/barbot/public/images/cubalibre.png");
+    }
+
+    @Test
+    public void testDeserializeSummary() throws Exception {
+
+        // Set View = Summary
+        useView(View.Summary.class, jacksonTester);
+
+        // Parse Json and compare with Recipe Object
+//        assertThat(this.jacksonTester.parse(recipeSummaryJson))
+//                .isEqualTo(recipeSummary);
+
+        Recipe result = this.jacksonTester.parseObject(recipeSummaryJson);
+
+        // Check Id
+        assertThat(result.getUid()).isEqualTo("recipe_8a4d7a");
+
+        // Check Name
+        assertThat(result.getName()).isEqualTo("Cuba Libre");
+
+        // Check Img
+        assertThat(result.getImageUrl()).isEqualTo("http://192.168.1.41/barbot/public/images/cubalibre.png");
+    }
+
+    @Test
+    public void testSerializeDetail() throws Exception {
+
+        // Set View = Detail
+        useView(View.Detail.class, jacksonTester);
+
+        JsonContent<Recipe> result = this.jacksonTester.write(recipeDetail);
+
+        // Compare Recipe Object to Json
+        //assertThat(result).isEqualToJson("recipedetail.json");
+
+        // Check Id
+        assertThat(result).hasJsonPathStringValue("@.recipe_id");
+        assertThat(result).extractingJsonPathStringValue("@.recipe_id")
+                .isEqualTo("recipe_9aa19a");
+
+        // Check Name
+        assertThat(result).hasJsonPathStringValue("@.name");
+        assertThat(result).extractingJsonPathStringValue("@.name")
+                .isEqualTo("The Sour");
+
+        // Check Img
+        assertThat(result).hasJsonPathStringValue("@.img");
+        assertThat(result).extractingJsonPathStringValue("@.img")
+                .isEqualTo("http://farm8.staticflickr.com/7252/7594170156_46bf574865_o.jpg");
+
+        // Check Ingredients
+        assertThat(result).hasJsonPathArrayValue("@.ingredients");
+    }
+
+    @Test
+    public void testDeserializeDetail() throws Exception {
+
+        // Set View = Detail
+        useView(View.Detail.class, jacksonTester);
+
+        // Parse Json and compare with Recipe Object
+//        assertThat(this.jacksonTester.parse(recipeDetailJson))
+//                .isEqualTo(recipeDetail);
+
+        Recipe result = this.jacksonTester.parseObject(recipeDetailJson);
+
+        // Check Id
+        assertThat(result.getUid()).isEqualTo("recipe_9aa19a");
+
+        // Check Name
+        assertThat(result.getName()).isEqualTo("The Sour");
+
+        // Check Img
+        assertThat(result.getImageUrl()).isEqualTo("http://farm8.staticflickr.com/7252/7594170156_46bf574865_o.jpg");
+
+        // Check Ingredients
+        //assertThat(result.getIngredients()).isEqualTo(ingredients);
+    }
+}
