f90e4e9d157ecd9196f5dccc9ef2ddbbd36054d4
Naveen Yadav <nyadav@uw.edu>
2017-12-18 16:24:09 -0800

    db migration script: added image_url column to category table

---
+++ sql/migrations/add_category_image_url_1.007.sql
@@ -0,0 +1,32 @@
+-- START OF UPDATE SCRIPT --
+-- Adds image_url column to the category table
+
+DROP PROCEDURE IF EXISTS `do_current_update_script`;
+
+DELIMITER //
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `do_current_update_script`(
+  migration VARCHAR(255),
+  version VARCHAR(50),
+  created_at DATETIME
+)
+BEGIN
+  DECLARE count_rows INT;
+  START TRANSACTION;
+  SET count_rows = (SELECT count(*) FROM `migration` m WHERE m.version = `version` COLLATE utf8_unicode_ci);
+  IF count_rows = 0 THEN
+
+    -- MIGRATION SCRIPT
+    ALTER TABLE `category`
+		ADD `image_url` varchar(255) COLLATE utf8_unicode_ci NOT NULL;
+    
+    INSERT INTO `migration`(migration,version,created_at,run_at)
+    VALUES(migration,version,created_at,NOW());
+  END IF;
+  COMMIT;
+END//
+DELIMITER ;
+
+CALL `do_current_update_script`('add_category_image_url', '1.007', '2017-12-18');
+
+-- END OF UPDATE SCRIPT --
